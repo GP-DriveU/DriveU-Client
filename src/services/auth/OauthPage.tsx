6f291439aee6f3333eb88c5af9781151837200dc
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-05-26 21:17:23 +0900

    feat: add oauth redirect

--- src/services/auth/OauthPage.tsx
+++ src/services/auth/OauthPage.tsx
@@ -1,1 +1,42 @@
-﻿
+﻿import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useFetchUser } from "../../hooks/auth/useFetchUser";
+import { exchangeGoogleCode } from "../../api/Login";
+
+function OAuthCallback() {
+  const redirectUri = `${window.location.origin}/oauth/google`;
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [refreshToken, setRefreshToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const code = searchParams.get("code");
+    if (!code) {
+      navigate("/login");
+      return;
+    }
+
+    exchangeGoogleCode(code, redirectUri)
+      .then(({ access_token, refresh_token }) => {
+        setAccessToken(access_token);
+        setRefreshToken(refresh_token);
+      })
+      .catch(() => {
+        navigate("/login");
+      });
+  }, [navigate, redirectUri, searchParams]);
+
+  useFetchUser(accessToken, refreshToken, () => {
+    navigate("/home");
+  });
+
+  return (
+    <>
+      <div className="bg-white"></div>
+    </>
+  );
+}
+
+export default OAuthCallback;
