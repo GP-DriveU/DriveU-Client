13a240bb7e6be9dc3611351475591b8bcfebec82
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-06-01 21:51:20 +0900

    feat: add list component

---
+++ src/commons/list/List.tsx
@@ -0,0 +1,31 @@
+﻿import ListItem, { type Item } from "./ListItem";
+
+interface ListProps {
+  items: Item[];
+  onToggleSelect: (id: string) => void;
+  onToggleFavorite: (id: string) => void;
+  selectable: boolean;
+}
+
+const List: React.FC<ListProps> = ({
+  items,
+  onToggleSelect,
+  onToggleFavorite,
+  selectable,
+}) => {
+  return (
+    <div className="p-4 rounded-md">
+      {items.map((item) => (
+        <ListItem
+          key={item.id}
+          item={item}
+          onToggleSelect={onToggleSelect}
+          onToggleFavorite={onToggleFavorite}
+          selectable={selectable}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default List;
