b90c1cd9ee96b70f78332b0762407f62f31ce36e
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-06-03 02:33:47 +0900

    feat: add titlesection component

---
+++ src/commons/section/TextSection.tsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+
+interface TextSectionProps {
+  title: string;
+  rightElement?: React.ReactNode;
+}
+
+const TextSection: React.FC<TextSectionProps> = ({ title, rightElement }) => {
+  return (
+    <div className="flex gap-6 items-center w-full px-10 py-6">
+      <div className="text-font text-2xl items-center font-semibold font-pretendard">
+        {title}
+      </div>
+      {rightElement && <div className="flex-grow">{rightElement}</div>}
+    </div>
+  );
+};
+
+export default TextSection;
