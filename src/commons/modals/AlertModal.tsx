bf6624a81bd50e9288e2be2941a11d81e431b2ac
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-06-03 22:30:33 +0900

    feat: add modal component

---
+++ src/commons/modals/AlertModal.tsx
@@ -0,0 +1,62 @@
+﻿import React from "react";
+import Button from "../inputs/Button";
+
+interface AlertModalProps {
+  title: string;
+  description: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+  isDanger?: boolean;
+  confirmText?: string;
+  cancelText?: string;
+}
+
+const AlertModal: React.FC<AlertModalProps> = ({
+  title,
+  description,
+  onConfirm,
+  onCancel,
+  isDanger = false,
+  confirmText,
+  cancelText,
+}) => {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-30">
+      <div className="w-full max-w-md px-6 py-6 bg-white rounded-xl flex flex-col items-center gap-6">
+        <div className="w-full text-center flex flex-col gap-4">
+          <div
+            className={`text-2xl font-bold ${
+              isDanger ? "text-danger" : "text-primary"
+            }`}
+          >
+            {title}
+          </div>
+          <div
+            className="text-base text-black font-normal"
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
+        </div>
+        <div className="w-full flex justify-center gap-4">
+          <Button
+            color="primary"
+            size="medium"
+            onClick={onCancel}
+            className="w-[130px] h-[55px] rounded-[15px]"
+          >
+            {cancelText || "취소"}
+          </Button>
+          <Button
+            color={isDanger ? "danger" : "primary"}
+            size="medium"
+            onClick={onConfirm}
+            className="w-[130px] h-[55px] rounded-[15px]"
+          >
+            {confirmText || "삭제"}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AlertModal;
