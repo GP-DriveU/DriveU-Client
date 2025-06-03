c4274b58697c2c4d41306e2fdf123448c2fe84f0
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-06-03 17:10:51 +0900

    chore: set md library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import typography from "@tailwindcss/typography";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   safelist: [
@@ -60,5 +62,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [typography],
 };
