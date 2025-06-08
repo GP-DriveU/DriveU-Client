6b7c9236c4cf1cc7bbaa803af1dadf9c7092f4a0
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-06-09 00:54:21 +0900

    feat: add summary api

---
+++ src/api/Summary.ts
@@ -0,0 +1,20 @@
+﻿import { http } from "./Fetch";
+
+interface SummaryResponse {
+  summaryId: number;
+  summary: string;
+}
+
+export const getNoteSummary = async (
+  noteId: number
+): Promise<SummaryResponse> => {
+  const response = await http.get<SummaryResponse>(`note/${noteId}/summary`);
+  return response.response;
+};
+
+export const createNoteSummary = async (
+  noteId: number
+): Promise<SummaryResponse> => {
+  const response = await http.post<SummaryResponse>(`note/${noteId}/summary`);
+  return response.response;
+};
