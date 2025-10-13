e8f299d0d12344f7bae7553832e28f8895d5758b
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-10-14 02:18:37 +0900

    feat: add trash page router

---
+++ src/services/trash/TrashPage.tsx
@@ -0,0 +1,11 @@
+﻿import TitleSection from "@/commons/section/TitleSection";
+
+function TrashPage() {
+  return (
+    <div className="w-full flex bg-white flex-col">
+      <TitleSection title="휴지통" semester="" />
+    </div>
+  );
+}
+
+export default TrashPage;
