3da24bceb52c309977ae756a240aa3459beefb74
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-11-29 22:14:22 +0900

    feature: add Link icon to itemUtils function

--- src/utils/itemUtils.tsx
+++ src/utils/itemUtils.tsx
@@ -1,8 +1,10 @@
-﻿import { IconAdd, IconDocs, IconNote } from "@/assets";
+﻿import { IconAdd, IconDocs, IconLink, IconNote } from "@/assets";
 import { type ItemType } from "@/types/Item";
 
 export const getIcon = (type: ItemType) => {
   switch (type) {
+    case "LINK":
+      return <IconLink />;
     case "FILE":
       return <IconDocs />;
     case "NOTE":
