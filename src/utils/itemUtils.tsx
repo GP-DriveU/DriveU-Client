56a7bbfcc327a1cbcd003f3c44c47e6f77c5be50
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-12-01 20:53:48 +0900

    refactor: add link types to icon utils function

--- src/utils/itemUtils.tsx
+++ src/utils/itemUtils.tsx
@@ -1,10 +1,24 @@
-﻿import { IconAdd, IconDocs, IconLink, IconNote } from "@/assets";
+﻿import {
+  IconAdd,
+  IconDocs,
+  IconLink,
+  IconNote,
+  IconGithub,
+  IconYoutube,
+} from "@/assets";
 import { type ItemType } from "@/types/Item";
 
-export const getIcon = (type: ItemType) => {
+export const getIcon = (type: ItemType, iconType?: string) => {
   switch (type) {
     case "LINK":
-      return <IconLink />;
+      switch (iconType) {
+        case "GITHUB":
+          return <IconGithub />;
+        case "YOUTUBE":
+          return <IconYoutube />;
+        default:
+          return <IconLink />;
+      }
     case "FILE":
       return <IconDocs />;
     case "NOTE":
