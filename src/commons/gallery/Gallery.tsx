7301ec386cb3be9d336571ae3da6c1a0b91c7a46
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-12-01 20:54:43 +0900

    style: fix gallery grid styles

--- src/commons/gallery/Gallery.tsx
+++ src/commons/gallery/Gallery.tsx
@@ -10,39 +10,38 @@ interface GalleryProps {
   onClickItem: (id: number) => void;
 }
 
-function Gallery({  
+function Gallery({
   items,
   onToggleSelect,
   onToggleFavorite,
   selectable,
-  onClickItem, }
-  : GalleryProps) {
+  onClickItem,
+}: GalleryProps) {
+  if (!items || items.length === 0) {
+    return (
+      <div className="w-full h-full flex flex-col py-6 items-center justify-center">
+        <EmptySection
+          title="저장된 파일 및 노트 없음"
+          subtitle="새로운 파일이나 노트를 업로드하세요."
+        />
+      </div>
+    );
+  }
 
   return (
-    <>
-      {items && items.length > 0 ? (
-        items.map((item) => (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16 px-4 py-6 w-full">
-            <GalleryItem
-              key={item.id}
-              item={item}
-              onToggleSelect={onToggleSelect}
-              onToggleFavorite={onToggleFavorite}
-              selectable={selectable}
-              onClickItem={onClickItem}
-            />
-          </div>
-        ))
-      ) : (
-        <div className="w-full h-full flex flex-col py-6 items-center justify-center">
-          <EmptySection
-            title="저장된 파일 및 노트 없음"
-            subtitle="새로운 파일이나 노트를 업로드하세요."
-          />
-        </div>
-      )}
-    </>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16 px-4 py-6 w-full">
+      {items.map((item) => (
+        <GalleryItem
+          key={item.id}
+          item={item}
+          onToggleSelect={onToggleSelect}
+          onToggleFavorite={onToggleFavorite}
+          selectable={selectable}
+          onClickItem={onClickItem}
+        />
+      ))}
+    </div>
   );
-};
+}
 
 export default Gallery;
