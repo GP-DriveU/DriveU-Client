cd7d2b7571e5db1ef1932f9a0d3b10b6459ed3ab
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-12-09 13:49:35 +0900

    feat: add storage parameter to trash api

--- src/api/Trash.ts
+++ src/api/Trash.ts
@@ -22,16 +22,19 @@ export interface ApiMessageResponse {
   message: string;
 }
 
+export interface TrashDeleteResponse {
+  message: string;
+  remainingStorage: number;
+}
+
 // 휴지통 파일 조회
 export const getTrashItems = async (
   types: string[],
   sort: TrashSortType
 ): Promise<GetTrashItemsResponse> => {
-  // URLSearchParams를 사용하여 쿼리 파라미터 생성
   const params = new URLSearchParams();
   params.append("sort", sort);
 
-  // 'all'이 아니거나 types 배열이 비어있지 않은 경우에만 type 파라미터 추가
   if (types.length > 0 && types[0].toUpperCase() !== "ALL") {
     types.forEach((type) => {
       params.append("type", type.toUpperCase());
@@ -56,16 +59,16 @@ export const getTrashDirectoryChildren = async (
 };
 
 // 휴지통 비우기
-export const emptyTrash = async (): Promise<ApiMessageResponse> => {
-  const response = await http.delete<ApiMessageResponse>("trash");
+export const emptyTrash = async (): Promise<TrashDeleteResponse> => {
+  const response = await http.delete<TrashDeleteResponse>("trash");
   return response.response;
 };
 
 // 휴지통 파일 지우기
 export const deleteTrashFile = async (
   resourceId: number
-): Promise<ApiMessageResponse> => {
-  const response = await http.delete<ApiMessageResponse>(
+): Promise<TrashDeleteResponse> => {
+  const response = await http.delete<TrashDeleteResponse>(
     `trash/resources/${resourceId}`
   );
   return response.response;
@@ -74,8 +77,8 @@ export const deleteTrashFile = async (
 // 휴지통 디렉토리 지우기
 export const deleteTrashDirectory = async (
   directoryId: number
-): Promise<ApiMessageResponse> => {
-  const response = await http.delete<ApiMessageResponse>(
+): Promise<TrashDeleteResponse> => {
+  const response = await http.delete<TrashDeleteResponse>(
     `trash/directories/${directoryId}`
   );
   return response.response;
